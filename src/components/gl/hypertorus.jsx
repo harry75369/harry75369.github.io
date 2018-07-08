ed03d191ded21002860a24e87768b589f7d6ab28
Chaoya Li <harry75369@gmail.com>
2018-07-08 09:25:28 +0800

    added a basic torus

--- src/components/gl/hypertorus.jsx
+++ src/components/gl/hypertorus.jsx
@@ -2,12 +2,51 @@ import React, { Component } from 'react';
 import {
   Color3,
   Vector3,
-  HemisphericLight,
+  Vector4,
   MeshBuilder,
+  VertexData,
+  StandardMaterial,
+  Mesh,
   Engine,
   Scene,
   ArcRotateCamera,
+  HemisphericLight,
 } from 'babylonjs';
+
+const DOUBLE_PI = 2 * Math.PI;
+const THIRD_PI = Math.PI / 3;
+
+function lerp(min, max, t) {
+  t = Math.min(1, Math.max(0, t));
+  return (1 - t) * min + t * max;
+}
+
+function calcColor(angle) {
+  const a = Math.abs(angle) % DOUBLE_PI;
+  const s = Math.floor(a / THIRD_PI) % 6;
+  const t = Math.min(1, a % THIRD_PI);
+  switch (s) {
+    case 0:
+      return { r: 1, g: t, b: 0 };
+    case 1:
+      return { r: 1 - t, g: 1, b: 0 };
+    case 2:
+      return { r: 0, g: 1, b: t };
+    case 3:
+      return { r: 0, g: 1 - t, b: 1 };
+    case 4:
+      return { r: t, g: 0, b: 1 };
+    case 5:
+      return { r: 1, g: 0, b: 1 - t };
+    default:
+      return { r: 0, g: 0, b: 0 };
+  }
+}
+
+function project4to3(vec) {
+  var w = vec.w + 2;
+  return new Vector3(vec.x / w, vec.y / w, vec.z / w);
+}
 
 class Hypertorus extends Component {
   addAxis(scene) {
@@ -36,6 +75,64 @@ class Hypertorus extends Component {
       scene,
     );
     z.color = Color3.Blue();
+    return { x, y, z };
+  }
+
+  addHypertorus(scene) {
+    const umin = 0;
+    const umax = 2 * Math.PI;
+    const vmin = 0;
+    const vmax = 2 * Math.PI;
+    const nu = 32;
+    const nv = 64;
+
+    for (let i = 0; i < nu; i += 1) {
+      if (i & (3 > 1)) continue;
+
+      const params = [];
+      const positions = [];
+      const colors = [];
+      for (let j = 0; j <= nv; j += 1) {
+        const v = lerp(vmin, vmax, j / nv);
+
+        for (let k = 0; k < 2; k += 1) {
+          const u = lerp(umin, umax, (i + k) / nu) + v / 4;
+
+          // point in 2d parametric space
+          params.push(u, v);
+
+          // positions in 4d and 3d
+          var p4 = new Vector4(
+            Math.cos(u),
+            Math.sin(u),
+            Math.cos(v),
+            Math.sin(v),
+          );
+          var p3 = project4to3(p4);
+          positions.push(p3.x, p3.y, p3.z);
+
+          // colors
+          var c = calcColor(u);
+          colors.push(c.r, c.g, c.b, 1.0);
+        }
+      }
+
+      const indices = [];
+      for (let j = 0; j + 2 < params.length / 2; j += 1) {
+        indices.push(j, j + 1, j + 2);
+      }
+
+      const mesh = new Mesh('mesh' + i, scene);
+      const material = new StandardMaterial();
+      material.backFaceCulling = false;
+      mesh.material = material;
+
+      const vertexData = new VertexData();
+      vertexData.indices = indices;
+      vertexData.positions = positions;
+      vertexData.colors = colors;
+      vertexData.applyToMesh(mesh, true);
+    }
   }
 
   buildScene(engine) {
@@ -50,25 +147,13 @@ class Hypertorus extends Component {
       Vector3.Zero(),
       scene,
     );
-    // camera.attachControl(engine.getRenderingCanvas());
+    camera.attachControl(engine.getRenderingCanvas());
 
     const light = new HemisphericLight('light', new Vector3(1, 1, 1), scene);
-    const object = MeshBuilder.CreateBox(
-      'box',
-      {
-        faceColors: [
-          new Color3(1, 0, 0),
-          new Color3(0, 1, 0),
-          new Color3(0, 0, 1),
-          new Color3(1, 1, 0),
-          new Color3(1, 0, 1),
-          new Color3(0, 1, 1),
-        ],
-      },
-      scene,
-    );
+
     this.addAxis(scene);
-    return { scene, camera, light, object };
+    this.addHypertorus(scene);
+    return { scene, camera };
   }
 
   componentDidMount() {
